8bbbeede8a6e2bc06c32636f80dcbcb9b6747ce4
Gregory Wilson <mistergwilson@gmail.com>
2013-04-14 09:00:06 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newpopplr::Application.config.secret_token = 'c7385cafba9e873891eeb46526c06651c74b50b2c67438794e9026ecfb125a52272a9f3e0f950b3f686e13247fe5cb1cc7013d031e091af239c3c21ffadf9d5f'
